ab16f006e91c2ffc5eb92496bcefcf869d4f515e
Matthew Heun <matthew.heun@me.com>
2023-06-06 08:03:07 -0400

    Update gcc compiler.

--- prep_R_software_on_arc.sh
+++ prep_R_software_on_arc.sh
@@ -9,5 +9,9 @@ echo "Installing R packages ..."
 # Activate the R environment
 conda activate r_env
 
+# Switch to updated gcc compiler
+module switch intel gnu/8.3.0
+
+# Now install R packages via R script
 Rscript install_R_packages.R
 
