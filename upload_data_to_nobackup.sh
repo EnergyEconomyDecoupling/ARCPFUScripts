1610ab992038b4bd530222fa1ef2794d1e699a56
Matthew Heun <matthew.heun@me.com>
2023-06-08 15:00:04 -0400

    Upload file.

--- upload_data_to_nobackup.sh
+++ upload_data_to_nobackup.sh
@@ -3,7 +3,7 @@
 # This script should be run from the user's local machine.
 # This script should NOT be run on ARC.
 
-echo "Upload InputData and OutputData to ARC nobackup directory"
+echo "Uploading InputData and OutputData to ARC /nobackup"
 
 # -a: archive mode
 # -z: compress files during transfer
@@ -11,4 +11,4 @@ echo "Upload InputData and OutputData to ARC nobackup directory"
 # -R: creates relative paths in destination
 # --delete deletes files on ARC4 if they have been deleted locally
 # /./ tells where to begin the copying
-rsync -azvR --delete -e ssh "/Users/mkh2/Dropbox/./Fellowship 1960-2015 PFU database/InputData/" "/Users/mkh2/Dropbox/./Fellowship 1960-2015 PFU database/OutputData/PipelineReleases" earmhe@arc4.leeds.ac.uk:"/nobackup/earmhe/"
+rsync -azvR --delete -e ssh "/Users/mkh2/Dropbox/./Fellowship 1960-2015 PFU database/InputData/" "/Users/mkh2/Dropbox/./Fellowship 1960-2015 PFU database/OutputData/PipelineReleases/" earmhe@arc4.leeds.ac.uk:"/nobackup/earmhe/"
